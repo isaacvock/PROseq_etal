2c2c209973d16d25c5a3a646fc4beb83d8f7efc5
isaacvock <isaac.vock@gmail.com>
2023-08-25 22:39:56 -0400

    pigz to stdout

--- workflow/scripts/pigz.py
+++ workflow/scripts/pigz.py
@@ -31,11 +31,11 @@ else:
     out2 = output[1]
 
     shell(
-        "(pigz -d -p {snakemake.threads} {fastq1} > {out1}) {log}"
+        "(pigz -d -p -c {snakemake.threads} {fastq1} > {out1}) {log}"
     ) 
 
     shell(
-        "(pigz -d -p {snakemake.threads} {fastq2} > {out2}) {log}"
+        "(pigz -d -p -c {snakemake.threads} {fastq2} > {out2}) {log}"
     )  
 
 
