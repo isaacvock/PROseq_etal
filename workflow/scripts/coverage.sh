a4caa99197866e5b98ffae2133b202713bd7bbbc
isaacvock <isaac.vock@gmail.com>
2023-11-19 13:46:03 -0500

    Hopefully proper passing of spaced argument

--- workflow/scripts/coverage.sh
+++ workflow/scripts/coverage.sh
@@ -2,6 +2,7 @@ normalize="$1"
 sample="$2"
 input="$3"
 output="$4"
+params=$5
 
 if [ "$normalize" = "True" ]; then
     normVal=$(awk -v sam=$sample '$1 == sam {print $2}' ./results/normalize/scale)
@@ -9,4 +10,4 @@ else
     normVal='1'
 fi
 
-bedtools genomecov "$5" -scale $normVal -ibam "$input" > "$output"
+bedtools genomecov $params -scale $normVal -ibam "$input" > "$output"
